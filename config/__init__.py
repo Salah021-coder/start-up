11c9a31115aa7297f6dacb50874ebdcb146c6779
Salah021-coder <mechakrasalah2003@gmail.com>
2026-01-30 20:37:31 +0100

    Delete config directory

--- config/__init__.py
+++
@@ -1,6 +0,0 @@
-from .settings import Settings
-from .criteria_config import CriteriaConfig
-from .gee_config import GEEConfig
-from .gemini_config import GeminiConfig
-
-__all__ = ['Settings', 'CriteriaConfig', 'GEEConfig', 'GeminiConfig']
